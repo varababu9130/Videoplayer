ac0dfd05bce1cf583d3337c9b99ede508f88078c
varababu9130 <i.varababu4@gmail.com>
2022-07-05 23:49:18 +0530

    init

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
